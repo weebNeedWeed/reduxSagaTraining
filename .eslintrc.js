b164adfe1228a5f4c53257856abbd5c991039004
weed <rivenmle@gmail.com>
2020-04-06 19:44:21 +0700

    [master] add some rules to eslint configure file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,40 @@
 const config = {
-  plugins: ["react", "react-hooks"],
-  rules: {
-    semi: ["error", "always"],
-    quotes: ["error", "double", { allowTemplateLiterals: true }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "no-console": "off",
-    "no-empty": ["warn", { allowEmptyCatch: true }],
-    "react/prop-types": "error",
-    "prettier/prettier": "off",
-  },
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  parser: "babel-eslint",
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
-    "prettier/react",
-    "prettier/standard",
-    "prettier"
-  ],
+	plugins: ["react", "react-hooks"],
+	rules: {
+		semi: ["error", "always"],
+		quotes: ["error", "double", { allowTemplateLiterals: true }],
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+		"no-console": "off",
+		"no-empty": ["warn", { allowEmptyCatch: true }],
+		"react/prop-types": "error",
+		"prettier/prettier": "off",
+		eqeqeq: "error",
+		"no-unused-vars": "warn",
+		"global-require": "warn",
+	},
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	parser: "babel-eslint",
+	env: {
+		browser: true,
+		node: true,
+		es6: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:prettier/recommended",
+		"prettier/react",
+		"prettier/standard",
+		"prettier",
+	],
 };
 
 module.exports = config;
