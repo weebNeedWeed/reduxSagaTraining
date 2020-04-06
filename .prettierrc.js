58468b84ac8c141f2963bb8402e982faf6ecdf2f
weed <rivenmle@gmail.com>
2020-04-06 19:43:44 +0700

    [master] missing commas

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 const config = {
-  semi: true,
-  trailingComma: "all",
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: "always",
-  quoteProps: "as-needed",
-  useTabs: true,
-  singleQuote: false,
-  jsxSingleQuote: false,
+	semi: true,
+	trailingComma: "all",
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: "always",
+	quoteProps: "as-needed",
+	useTabs: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
 };
 module.exports = config;
