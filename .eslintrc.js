2f86f3b064e8fc16bae47147e1c95bd98a7d95b7
weed <rivenmle@gmail.com>
2020-04-06 12:45:49 +0700

    configure eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+const config = {
+  plugins: ["react", "react-hooks"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "no-console": "off",
+    "no-empty": ["warn", { allowEmptyCatch: true }],
+    "react/prop-types": "error",
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+};
+
+module.exports = config;
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+const config = {
+	semi: true,
+	trailingComma: "all",
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: "always",
+	parser: "babel",
+	quoteProps: "as-needed",
+	useTabs: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+};
+module.exports = config;
